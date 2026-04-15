a17470ec6740448b059394d0168f3f8718393781
Rishi Ramani <rishiramani1512@gmail.com>
2026-04-15 20:55:27 +0530

    Fixed rate limiter

--- middleware/ratelimiter.js
+++ middleware/ratelimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit"
 
 // Limit: 100 requests per 15 minutes per IP
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 3 * 60 * 1000, // 3 minutes
   max: 100, // limit each IP to 100 requests per window
   standardHeaders: true, // Return rate limit info in `RateLimit-*` headers
   legacyHeaders: false, // Disable `X-RateLimit-*` headers
